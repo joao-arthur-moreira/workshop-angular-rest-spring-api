8ccfbf3784377f021bbb766087f4417f2fcbc8ce
drlingerie <ti3@drling.com.br>
2018-02-28 16:51:24 -0300

    Adicionado o requestMapping para não precisa utilizar nos métodos

--- src/main/java/com/jajm/patrimonio/resource/ItemResource.java
+++ src/main/java/com/jajm/patrimonio/resource/ItemResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jajm.patrimonio.model.Item;
@@ -16,17 +17,18 @@ import com.jajm.patrimonio.repository.ItemRepository;
 
 @RestController
 @CrossOrigin("${origem-permitida}")
+@RequestMapping("/itens")
 public class ItemResource {
 	
 	@Autowired
 	private ItemRepository itemRepository;
 	
-	@GetMapping("/itens")
+	@GetMapping
 	public List<Item> listar() {
 		return itemRepository.findAll();
 	}
 	
-	@PostMapping("/itens")
+	@PostMapping
 	public Item adicionar(@RequestBody @Valid Item item) {
 		return itemRepository.save(item);
 	}
